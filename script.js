44ec4c3df73af832b67ac8d8fd922f80ef67297f
Nicole Menzel <nicolemenzel@icloud.com>
2022-11-17 12:00:56 +0100

    added key control

---
+++ script.js
@@ -0,0 +1,7 @@
+function handleKeyDown(ev){
+    //console.log(ev)
+    const player = document.querySelector("#player")
+    player.setAttribute("cx", 100 * Math.random)
+    player.setAttribute("cy" , 100 * Math.random)
+}
+window.onkeydown = handleKeyDown
